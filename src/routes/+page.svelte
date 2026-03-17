a03640f49f2b803617323aaeae8c8d2119a12383
Nang Eaindra Hein <eaindraann@gmail.com>
2026-03-17 15:10:55 +0700

    chore(ui):fix screen size(add responsive style)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,17 +27,17 @@
     <img
       src="/images/pattern-bg-desktop.png"
       alt="pattern-bg"
-      class=" w-full h-60 object-cover"
+      class=" w-full h-80 lg:h-60 object-cover"
     />
     <!--title-->
     <h1
-      class="absolute text-2xl pt-4 font-semibold z-10 top-0 text-white translate-x-1/2 right-70 inset-x-0"
+      class="absolute text-xl text-nowrap pt-4 font-semibold z-10 top-0 text-white translate-x-1/2 left-10 md:left-50 lg:left-100 xl:left-140"
     >
       IP Address Tracker
     </h1>
     <!--input-->
     <div
-      class="cursor-pointer flex rounded-xl h-10 bg-white absolute z-10 top-20 translate-x-1/2 w-100 left-80"
+      class="cursor-pointer flex rounded-xl h-15 md:h-10 bg-white absolute z-10 top-20 translate-x-1/2 w-100 -left-45 md:left-0 lg:left-50 xl:left-80"
     >
       <input
         bind:value={ipAddressKey}
@@ -47,15 +47,104 @@
       />
       <button
         onclick={() => {}}
-        class="bg-gray-700 px-4 h-10 rounded-r-xl text-white cursor-pointer"
+        class="bg-gray-700 px-4 h-15 md:h-10 rounded-r-xl text-white cursor-pointer"
         ><IconamoonArrowRight2 class="w-5 h-5" /></button
       >
     </div>
   </div>
   <!--data-->
-
+  <!--for sm screen -->
   <div
-    class="divide-x divide-gray-300 absolute z-10 h-fit bg-white grid grid-cols-4 gap-5 p-6 top-40 left-50 right-50 rounded-xl shadow-xl"
+    class="lg:hidden top-40 min-w-90 -left-35 md:min-w-130 text-nowrap translate-x-1/2 rounded-xl shadow-xl h-fit grid place-items-center text-center gap-5 absolute bg-white z-10 p-6"
+  >
+    <div class=" text-xs text-gray-500 font-semibold">
+      IP ADDRESS
+      {#if geoQuery.isLoading}
+        <div
+          class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Loading ...
+        </div>
+      {:else if geoQuery.isError}
+        <div
+          class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Failed
+        </div>{:else if geoQuery.isSuccess}
+        <div class=" text-lg pt-4 font-semibold text-black">
+          <div class="text-sm text-gray-500">ip address:</div>
+          {geoQuery.data.ip}
+          <div class="text-sm text-gray-500">latitude.longitude:</div>
+          {geoQuery.data.location.lat}.{geoQuery.data.location.lng}
+        </div>
+      {/if}
+    </div>
+
+    <div class=" text-xs text-gray-500 font-semibold">
+      LOCATION {#if geoQuery.isLoading}
+        <div
+          class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Loading ...
+        </div>
+      {:else if geoQuery.isError}
+        <div
+          class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Failed
+        </div>
+      {:else if geoQuery.isSuccess}
+        <div class=" text-lg pt-4 font-semibold text-black">
+          {geoQuery.data.location.country}
+          {#if geoQuery.data.location.region || geoQuery.data.location.city}
+            {#if geoQuery.data.location.region}
+              ,{geoQuery.data.location.region}
+              {#if geoQuery.data.location.city},{geoQuery.data.location
+                  .city}{/if}{/if}
+          {:else}{/if}
+        </div>
+      {/if}
+    </div>
+    <div class=" text-xs text-gray-500 font-semibold">
+      TIMEZONE {#if geoQuery.isLoading}
+        <div
+          class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Loading ...
+        </div>
+      {:else if geoQuery.isError}
+        <div
+          class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Failed
+        </div>{:else if geoQuery.isSuccess}
+        <div class=" text-lg pt-4 font-semibold text-black">
+          UTC {geoQuery.data.location.timezone}
+        </div>
+      {/if}
+    </div>
+    <div class=" text-xs text-gray-500 font-semibold">
+      ISP {#if geoQuery.isLoading}
+        <div
+          class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Loading ...
+        </div>
+      {:else if geoQuery.isError}
+        <div
+          class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-2 text-black w-fit text-sm"
+        >
+          Failed
+        </div>{:else if geoQuery.isSuccess}
+        <div class=" text-lg pt-4 font-semibold text-black">
+          {geoQuery.data.isp}
+        </div>
+      {/if}
+    </div>
+  </div>
+  <!--for lg screen above-->
+  <div
+    class="hidden lg:block divide-x divide-gray-300 absolute z-10 h-fit bg-white lg:grid grid-cols-4 gap-5 p-6 top-40 left-50 right-50 rounded-xl shadow-xl"
   >
     <div class=" text-xs text-gray-500 font-semibold">
       IP ADDRESS
@@ -147,13 +236,13 @@
   <div class="w-full relative grid place-items-center z-0">
     {#if geoQuery.isPending}
       <div
-        class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-30 text-black w-fit text-sm"
+        class="border border-t-blue-300 border-x-blue-400 border-b-blue-500 rounded-lg px-4 py-1 mt-50 lg:mt-30 text-black w-fit text-sm"
       >
         Loading ...
       </div>
     {:else if geoQuery.isError}
       <div
-        class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-30 text-black w-fit text-sm"
+        class="border border-x-red-500 border-t-red-300 border-b-red-600 rounded-lg px-4 py-1 mt-50 lg:mt-30 text-black w-fit text-sm"
       >
         Failed
       </div>{:else}
